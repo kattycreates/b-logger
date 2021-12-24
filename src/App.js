36eaa1c988200cb79a4229212bc6b642dcf4b659
kattycreates <iamkarthika7@gmail.com>
2021-12-24 22:57:06 +0530

    Added files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
+import {useState} from 'react';
 import './App.css';
+import bookData from './data';
+import Header from './components/Header';
+import Books from './components/Books';
 
 function App() {
+  const [books,setBooks]=useState(bookData);
+  console.log(books);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <Header setBooks={setBooks} bookData={bookData} />
+      <Books books={books} setBooks={setBooks}/>  
     </div>
-  );
+    
+  )
 }
 
 export default App;
+
